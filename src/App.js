fdcad650ab6ed00bca8c0ffeaee8cbadd55c6a18
Ricardo Leite <ricardo.leitejr95@gmail.com>
2020-07-27 09:52:20 -0600

    Simple wireframing for rules component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import NavBar from "./components/Navigation/NavBar";
 import GridContainer from "./components/GridContainer/GridContainer";
+import Rules from "./components/Rules/Rules";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <h1>Let's Populate Mars!</h1>
       <GridContainer />
+      <Rules />
     </div>
   );
 }
--- src/components/GridContainer/GridContainer.js
+++ src/components/GridContainer/GridContainer.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Grid from "../Grid/Grid";
+import Rules from "../Rules/Rules";
 const GridContainer = () => {
   return (
     <div
       style={{
         display: "flex",
         flexDirection: "row",
-        width: "80%",
+        width: "70%",
+        padding: "25px",
         margin: "0 auto"
       }}
     >
