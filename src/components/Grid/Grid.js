3bda8c787ea1c9bd09ab0247904eb28c8081b2c8
Ricardo Leite <ricardo.leitejr95@gmail.com>
2020-07-27 14:54:09 -0600

    Grid simulation 1st level working

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,9 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useCallback } from "react";
 import { Board } from "./GridStyles";
 import { produce } from "immer";
 
 const numRows = 25;
-const numCols = 25;
+const numCols = 35;
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0]
+];
 
 const Grid = () => {
   const [grid, setGrid] = useState(() => {
@@ -13,6 +24,40 @@ const Grid = () => {
     }
     return rows;
   });
+  const [start, setStart] = useState(false); // Hold state for running algorithms
+
+  const startRef = useRef(start);
+  startRef.current = start;
+
+  const startSimulation = useCallback(() => {
+    if (!startRef.current) {
+      return;
+    }
+
+    setGrid(grid => {
+      return produce(grid, gridCopy => {
+        for (let i = 0; i < numRows; i++) {
+          for (let j = 0; j < numCols; j++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newJ = j + y;
+              if (newI >= 0 && newI < numRows && newJ >= 0 && newJ < numCols) {
+                neighbors += grid[newI][newJ];
+              }
+            });
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][j] = 0;
+            } else if (grid[i][j] === 0 && neighbors === 3) {
+              gridCopy[i][j] = 1;
+            }
+          }
+        }
+      });
+    });
+    setTimeout(startSimulation, 500);
+  }, []);
 
   const width = 20;
   const height = 20;
@@ -46,6 +91,18 @@ const Grid = () => {
             />
           ))
         )}
+        <button
+          style={{ width: "40px" }}
+          onClick={() => {
+            setStart(!start);
+            if (!start) {
+              startRef.current = true;
+              startSimulation();
+            }
+          }}
+        >
+          {start ? "stop" : "start"}
+        </button>
       </div>
     </>
   );
